ba0b244a227e8b32d1e28258bb167a7e6823c196
RebeccaSalles <rebeccapsalles@gmail.com>
2018-12-21 14:32:00 -0200

    dev51

--- dev/18.09/Tests-UseCase-1.r
+++ dev/18.09/Tests-UseCase-1.r
@@ -10,6 +10,9 @@ loadlibrary <- function(x)
 loadlibrary("RSNNS")
 loadlibrary("TSPred")
 
+loadlibrary("foreach")
+install.packages("doParallel")
+
 generate_candidate_tspred <- function(candidate,data,test_len=20,prep_test=TRUE,onestep=FALSE,eval_fitness=FALSE){
   lyr1 <- candidate$size_lyr_1
   lyr2 <- candidate$size_lyr_2
@@ -35,10 +38,15 @@ generate_candidate_tspred <- function(candidate,data,test_len=20,prep_test=TRUE,
   return(tspred_candidate)
 }
 
-benchmark_hiperpar <- function(hiperpar,...,rank.by=c("MSE")){
+benchmark_hiperpar <- function(hiperpar,cl,log_file,...,rank.by=c("MSE")){
+  #browser()
+  registerDoParallel(cl)
   
-  rank <- data.frame()
-  for(candidate in 1:nrow(hiperpar)){
+  require(plyr)
+  
+  rank <- foreach(candidate=1:nrow(hiperpar), .combine=rbind.fill) %dopar% {
+    cat("Running TSPred workflow with candidate hiperparameters",candidate,"of",nrow(hiperpar),"\n", file = log_file, append = TRUE,sep=" ")
+    capture.output(hiperpar[candidate,], file = log_file, append = TRUE)
     
     obj <- generate_candidate_tspred(hiperpar[candidate,],...)
     
@@ -63,9 +71,14 @@ benchmark_hiperpar <- function(hiperpar,...,rank.by=c("MSE")){
       rank_obj <- cbind(rank_obj,error)
     }
     
-    require(plyr)
-    rank <- rbind.fill(rank,rank_obj)
+    cat("\n","Results:","\n", file = log_file, append = TRUE,sep="")
+    capture.output(rank_obj, file = log_file, append = TRUE)
+    
+    rank_obj
   }
+  
+  #stop cluster
+  stopCluster(cl)
   
   rownames(rank) <- NULL
   
@@ -98,15 +111,25 @@ eval_fitness <- FALSE
 MM <- MinMax(byRow=TRUE)
 
 #Hiperparameters:
-size_lyr_1 <- seq(2,20)
-size_lyr_2 <- c(NA,seq(0,20))
-learnFuncParams <- seq(0.1,1,0.1)
+#size_lyr_1 <- seq(2,20)
+#size_lyr_2 <- c(NA,seq(0,20))
+#learnFuncParams <- seq(0.1,1,0.1)
+#maxit <- c(1000,5000,10000)
+
+size_lyr_1 <- seq(2,3)
+size_lyr_2 <- c(NA,seq(0,3))
+learnFuncParams <- seq(0.1,1,0.5)
 maxit <- c(1000,5000,10000)
 
 hiperpar <- expand.grid(size_lyr_1=size_lyr_1, size_lyr_2=size_lyr_2, learnFuncParams=learnFuncParams, maxit=maxit)
 
+#setup parallel backend to use many processors
+cores <- detectCores()
+cl <- makeCluster(cores[1]-1) #not to overload your computer
+
+log_file <- "/home/rebecca/TSPred/dev/18.09/log_usecase1.txt"
 
 bmrk_usecase_1 <- list()
 for(ts in names(CATS)){
-  bmrk_usecase_1[[ts]] <- benchmark_hiperpar(hiperpar,data=CATS[ts],test_len=test_len,prep_test=prep_test,onestep=onestep,eval_fitness=eval_fitness)
+  bmrk_usecase_1[[ts]] <- benchmark_hiperpar(hiperpar,cl,log_file,data=CATS[ts],test_len=test_len,prep_test=prep_test,onestep=onestep,eval_fitness=eval_fitness)
 }
